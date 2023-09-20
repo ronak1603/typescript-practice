d5b84eae33559acc8b9d26cd1e1ec1b7b8123187
Ronak Malani <ronakm@gluelabs.com>
2023-09-20 13:07:55 +0530

    typecript examples in 2 & 3 lesson

---
+++ lesson-2/src/main.ts
@@ -0,0 +1,20 @@
+let myName: string = 'Dave'
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = 'John'
+meaningOfLife = 42
+isLoading = true
+album = 5150
+
+
+const sum = (a: number, b: string) => {
+    console.log(a + b)
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+sum(4, 'ronak')
+let re: RegExp = /\w+/g
---
+++ lesson-3/src/main.ts
@@ -0,0 +1,57 @@
+let bikes = ['harley davidson', 'hunter', 350];
+let mixedArr = ['drake', 21, 'Savage', false];   //doubt
+
+bikes.push('Himalayan gt');
+mixedArr.unshift(true)
+
+bikes[0] = 24
+
+let cars: string[] = ['polo gt', 'supra']
+cars.push('challenger')
+let bands: [string, boolean, number] = ['the 1975', true, 1975]
+
+// Objects
+let obj: object
+obj = []
+console.log(typeof obj)
+obj = bands
+obj = {}
+
+interface common{
+    name:string;
+    active?:boolean
+    albums: (string | number)[]
+}
+
+let obj1 : common = {
+    name:'Drake',
+    albums : ['Scorpian', 'nevermind', 2011]
+}
+
+let obj2 : common = {
+    name:'post malone',
+    albums : ['beerbongs', 'austin', 2011],
+    active: true
+}
+
+const obj3 = (prop: common) => {
+    if(prop.name){
+        return `hello ${prop.name.toUpperCase()}!`
+    }
+
+    return 'bye!'
+}
+
+console.log(obj3(obj1));
+
+//Enums
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.U)
+
