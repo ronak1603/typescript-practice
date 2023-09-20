20a3cbe7caf474f5b59fd42d9dc5fc4f36d356ed
Ronak Malani <ronakm@gluelabs.com>
2023-09-20 11:58:10 +0530

    lesson 1

---
+++ lesson-1/src/main.ts
@@ -0,0 +1,10 @@
+let username = 'Dave'
+console.log(username)
+
+let a: number = 12
+let b: string = '6'
+let c: number = 2
+
+console.log(a / b) //error of type
+
+console.log(c * b) //error of type
